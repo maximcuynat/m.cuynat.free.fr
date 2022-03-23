cdffa0deaa42f7d72121a352d89a1500b73e3db9
[CUYNAT Maxim] <[maxim.cuynat@viacesi.fr]>
2022-03-23 10:44:57 +0100

    flem de commmebter

--- View/Acceuil.php
+++ View/Acceuil.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CUYNAT Maxim</title>
-</head>
-<body>
-
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+		<title>CUYNAT Maxim</title>
+	</head>
+	<body class="bg-asphalt">
+		<header>
+			<div class="responsive display-flex al-it-center flex-column">
+				<div>
+					<h1 class="text-white display-1">Mon rêve, l'espace</h1>
+				</div>
+				<div>
+					<p>Ediant en deuxième année chez CESI école d'ingénieurs</p>
+				</div>
+			</div>
+		</header>
+	</body>
 </html>
